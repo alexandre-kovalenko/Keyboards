927977458cf3d9a847192a974497e6bc1ecc7911
Alexandre 'Sunny' Kovalenko <techxorcist@outlook.com>
2025-07-27 11:20:20 -0400

    Added ukrainian layout and IME for Chrome OS

---
+++ ChromeOS/ukr/README.md
@@ -0,0 +1,16 @@
+# Sunny-branded ukrainian phonetic keyboard layout
+ Sunny-branded ukrainian phonetic keyboard layout and input method for Chrome OS.
+
+Based on https://github.com/google/extra-keyboards-for-chrome-os/tree/master
+
+## Installation
+
+### GitHub
+
+1. Open a new tab and go to chrome://extensions.
+2. Toggle "Developer mode" on (it is in the top right corner of the extensions
+   page).
+3. Click "Load unpacked extensions...".
+4. Pick the directory containing the manifest for the extension you want to
+   enable and click "Open".
+5. Go to "Settings/Device/Keyboard and inputs/Input settings" and add input method to the input methods.
---
+++ ChromeOS/ukr/background.js
@@ -0,0 +1,125 @@
+/*
+Sunny-branded russian phonetic layout for ChromeOS
+*/
+
+let contextID = 0;
+
+const capsoffkeys = [
+    "Digit1",
+    "Digit2",
+    "Digit3",
+    "Digit4",
+    "Digit5",
+    "Digit6",
+    "Digit7",
+    "Digit8",
+    "Digit9",
+    "Digit0",
+    "Minus",
+    "Equal",
+    "Backslash",
+    "Slash"
+];
+
+//The lookup table (lut) object structure:
+//  "keyData.code"    : [ "plain", "shifted", "altered", "altered&shifted" ]
+const lut = {
+    "Backquote"   : [ "ю", "Ю", "`", " ́" ],
+    "Digit1"      : [ "1", "!", "₽", "¹" ],
+    "Digit2"      : [ "2", "@","@", "²" ],
+    "Digit3"      : [ "3", "ї", "№", "³" ],
+    "Digit4"      : [ "4", "Ї", "$", "¤" ],
+    "Digit5"      : [ "5", "ґ", "€", "£" ],
+    "Digit6"      : [ "6", "Ґ", "^", "¼" ],
+    "Digit7"      : [ "7", "&", "&", "½" ],
+    "Digit8"      : [ "8", "*", "~", "¾" ],
+    "Digit9"      : [ "9", "(", "«", "±" ],
+    "Digit0"      : [ "0", ")", "»", "™" ],
+    "Minus"       : [ "-", "_", "¥", "₴" ],
+    "Equal"       : [ "ч", "Ч", "×", "÷" ],
+    "KeyQ"        : [ "я", "Я",  "",  "" ],
+    "KeyW"        : [ "в", "В", "џ", "Џ" ],
+    "KeyE"        : [ "е", "Е",  "",  "" ],
+    "KeyR"        : [ "р", "Р", "¶", "®" ],
+    "KeyT"        : [ "т", "Т", "є", "Є" ],
+    "KeyY"        : [ "и", "И", "њ", "Њ" ],
+    "KeyU"        : [ "у", "У", "ґ", "Ґ" ],
+    "KeyI"        : [ "і", "І", "ї", "Ї" ],
+    "KeyO"        : [ "о", "О", "ў", "Ў" ],
+    "KeyP"        : [ "п", "П",  "",  "" ],
+    "BracketLeft" : [ "ш", "Ш", "{", "“" ],
+    "BracketRight": [ "щ", "Щ", "}", "”" ],
+    "Backslash"   : [ "є", "Є", "|", "¬" ],
+    "KeyA"        : [ "а", "А",  "",  "" ],
+    "KeyS"        : [ "с", "С", "§", "§" ],
+    "KeyD"        : [ "д", "Д",  "",  "" ],
+    "KeyF"        : [ "ф", "Ф",  "", "ª" ],
+    "KeyG"        : [ "г", "Г", "Ω",  "" ],
+    "KeyH"        : [ "х", "Х",  "",  "" ],
+    "KeyJ"        : [ "й", "Й", "j", "J" ],
+    "KeyK"        : [ "к", "К", "љ", "Љ" ],
+    "KeyL"        : [ "л", "Л", "ђ", "Ђ" ],
+    "Semicolon"   : [ ";", ":", "[", "‘" ],
+    "Quote"       : [ "'", "\"", "]", "’" ],
+    "KeyZ"        : [ "з", "З", "s", "S" ],
+    "KeyX"        : [ "ь", "Ь", "ћ", "Ћ" ],
+    "KeyC"        : [ "ц", "Ц", "¢", "©" ],
+    "KeyV"        : [ "ж", "Ж", "↓", "↑" ],
+    "KeyB"        : [ "б", "Б", "i", "I" ],
+    "KeyN"        : [ "н", "Н",  "",  "" ],
+    "KeyM"        : [ "м", "М", "µ", "º" ],
+    "Comma"       : [ ",", "<", "<", "←" ],
+    "Period"      : [ ".", ">", ">", "→" ],
+    "Slash"       : [ "/", "?", "'", "°" ],
+    "Space"       : [ " ", " ", "\u00a0", "\u00a0" ],
+};
+
+let altstate = false;
+
+chrome.input.ime.onFocus.addListener(
+    function(context) {
+      contextID = context.contextID;
+    }
+);
+
+chrome.input.ime.onBlur.addListener(() => {
+  contextID = 0;
+})
+
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+      let handled = false;
+
+      if ((keyData.type == "keydown") && (keyData.code == "AltRight")) {
+          if (altstate == false) {
+              altstate = true;
+          }
+      }
+
+      if (keyData.type == "keydown" && keyData.altKey == false && keyData.ctrlKey == false) {
+
+          if (lut[keyData.code]) {
+            let modified = (keyData.shiftKey != keyData.capsLock) + 2 * altstate;
+            if (capsoffkeys.includes(keyData.code)) {
+              modified = keyData.shiftKey + 2 * altstate;
+            }
+              
+            let emit = lut[keyData.code][modified];
+            altstate = false;
+              
+          if (emit != null && contextID != 0) {
+            chrome.input.ime.commitText({
+              "contextID": contextID,
+              "text": emit,
+            }, () => {
+              if (chrome.runtime.lastError) {
+                console.error('Error committing text:', chrome.runtime.lastError);
+                return;
+              }
+            });
+          }
+          handled = true;
+        }
+      }
+      return handled;
+});
